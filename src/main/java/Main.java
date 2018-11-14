7b14368d74478df3c27e4d6553e255f1c25b7223
akyulas <bolshack_fire@hotmail.com>
2018-11-14 19:05:34 +0800

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+/**
+ * Created by jodiakyulas on 14/11/18.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Arguments! (" + Arrays.toString(args) + ")");
+    }
+
+}
